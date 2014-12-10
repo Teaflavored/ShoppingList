97657b7d72786ffbb852ea1fde7185dd24c1c1e5
Auster Chen <Auster.s.chen@gmail.com>
2014-12-09 16:01:01 -0800

    initial commit with angular setup

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require angular
+//= require angular-rails-templates
+
+//= require shopping_list/app
+
+//= require_tree ./shopping_list/templates
+//= require_tree ./shopping_list/modules
+//= require_tree ./shopping_list/filters
+//= require_tree ./shopping_list/directives
+//= require_tree ./shopping_list/models
+//= require_tree ./shopping_list/services
+//= require_tree ./shopping_list/controllers
---
+++ app/assets/javascripts/shopping_list/app.js
@@ -0,0 +1,10 @@
+var app = angular.module("app", [
+  "templates"
+])
+app.config([
+
+  ])
+
+app.run(function(){
+  console.log("angular app running")
+})
